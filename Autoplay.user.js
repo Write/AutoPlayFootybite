838a857dbe7d3dfaaf92abd2145cd94900cd926d
Write <541722+Write@users.noreply.github.com>
2022-02-20 20:54:02 +0100

    Add Anti-Anti Right-click for Myoplay.club

--- Autoplay.user.js
+++ Autoplay.user.js
@@ -41,7 +41,7 @@
 // @match       *://ragnaru.net/*
 // @match       *://nowlive.pro/*
 // @grant       none
-// @version     6.0
+// @version     6.1
 // @author      -
 // ==/UserScript==
 
@@ -499,6 +499,136 @@
       });
     })
     
+    
+  /* ----------------------- */
+  /* Myoplay anti-anti-rightclick */
+  /* ----------------------- */
+    window.addEventListener('contextmenu', function contextmenu(event) {
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+        var handler = new eventHandler(event);
+        window.removeEventListener(event.type, contextmenu, true);
+        var eventsCallBack = new eventsCall(function() {});
+        handler.fire();
+        window.addEventListener(event.type, contextmenu, true);
+        if (handler.isCanceled && (eventsCallBack.isCalled)) {
+            event.preventDefault();
+        }
+    }, true);
+
+
+    if (window && typeof window != undefined) {
+
+        var doc = document;
+        var body = document.body;
+
+        var docEvents = [
+            doc.oncontextmenu = null,
+            doc.onselectstart = null,
+            doc.ondragstart = null,
+            doc.onmousedown = null
+        ];
+
+        var bodyEvents = [
+            body.oncontextmenu = null,
+            body.onselectstart = null,
+            body.ondragstart = null,
+            body.onmousedown = null,
+            body.oncut = null,
+            body.oncopy = null,
+            body.onpaste = null
+        ];
+
+        [].forEach.call(
+            ['copy', 'cut', 'paste', 'select', 'selectstart'],
+            function(event) {
+                document.addEventListener(event, function(e) { e.stopPropagation(); }, true);
+            }
+        );
+
+        [].forEach.call(
+            ['click', 'contextmenu', 'copy', 'cut', 'paste', 'mouseup', 'mousedown', 'keyup', 'keydown', 'drag', 'dragstart', 'select', 'selectstart'],
+            function(event) {
+                document.addEventListener(event, function(e) { e.stopPropagation(); }, true);
+            }
+        );
+
+        window.addEventListener('devtoolschange', function contextmenu(event) {
+            event.stopPropagation();
+            event.stopImmediatePropagation();
+            var handler = new eventHandler(event);
+            window.removeEventListener(event.type, contextmenu, true);
+            var eventsCallBack = new eventsCall(function() {});
+            handler.fire();
+            window.addEventListener(event.type, contextmenu, true);
+            if (handler.isCanceled && (eventsCallBack.isCalled)) {
+                event.preventDefault();
+            }
+        }, true);
+
+        window.addEventListener('contextmenu', function contextmenu(event) {
+            event.stopPropagation();
+            event.stopImmediatePropagation();
+            var handler = new eventHandler(event);
+            window.removeEventListener(event.type, contextmenu, true);
+            var eventsCallBack = new eventsCall(function() {});
+            handler.fire();
+            window.addEventListener(event.type, contextmenu, true);
+            if (handler.isCanceled && (eventsCallBack.isCalled)) {
+                event.preventDefault();
+            }
+        }, true);
+    }
+
+    function eventsCall() {
+        this.events = ['DOMAttrModified', 'DOMNodeInserted', 'DOMNodeRemoved', 'DOMCharacterDataModified', 'DOMSubtreeModified'];
+        this.bind();
+    }
+
+    eventsCall.prototype.bind = function() {
+        this.events.forEach(function (event) {
+            document.addEventListener(event, this, true);
+        }.bind(this));
+    };
+
+    eventsCall.prototype.handleEvent = function() {
+        this.isCalled = true;
+    };
+
+    eventsCall.prototype.unbind = function() {
+        this.events.forEach(function (event) {}.bind(this));
+    };
+
+    function eventHandler(event) {
+        this.event = event;
+        this.contextmenuEvent = this.createEvent(this.event.type);
+    }
+
+    eventHandler.prototype.createEvent = function(type) {
+        var target = this.event.target;
+        var event = target.ownerDocument.createEvent('MouseEvents');
+        event.initMouseEvent(
+            type, this.event.bubbles, this.event.cancelable,
+            target.ownerDocument.defaultView, this.event.detail,
+            this.event.screenX, this.event.screenY, this.event.clientX, this.event.clientY,
+            this.event.ctrlKey, this.event.altKey, this.event.shiftKey, this.event.metaKey,
+            this.event.button, this.event.relatedTarget
+        );
+        return event;
+    };
+
+    eventHandler.prototype.fire = function() {
+        var target = this.event.target;
+        var contextmenuHandler = function(event) {
+            event.preventDefault();
+        }.bind(this);
+        target.dispatchEvent(this.contextmenuEvent);
+        this.isCanceled = this.contextmenuEvent.defaultPrevented;
+    };
+    /* -------------------------------- */
+    /* END Myoplay anti-anti-rightclick */
+    /* -------------------------------- */
+    
     var myoplayStyle = `
     .eae-particle-wrapper {
       display: none;
@@ -506,7 +636,7 @@
     `
     pasteStyle(myoplayStyle);
     
-  }
+  } // End Myoplay
   
 
   /* ----------------------- */
